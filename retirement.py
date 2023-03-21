8ed11ad7cbcde68abf4a16c7816211bd9d9920bd
andrew <andrew.j.kuklinski@gmail.com>
2023-03-20 19:44:26 -0600

    need to figure out how to increase the number of array indicies when puchase goal is reached

--- retirement.py
+++ retirement.py
@@ -1,15 +1,19 @@
 class Business:
     #current_profit = 0
-    def __init__(self, name, monthlyIncome, increasePerYear):
+    def __init__(self, name, monthlyIncome, increasePerYear, startAmount):
         self.name = name
         self.monthlyIncome = monthlyIncome
         self.increasePerYear = increasePerYear
+        self.startAmount = startAmount
     
     def calc_new_monthlyIncome(self):
         self.monthlyIncome *= (1 + self.increasePerYear)
     
     def get_monthly_income(self):
         return self.monthlyIncome
+    
+    def get_start_amount(self):
+        return self.startAmount
 
 class Savings:
 
@@ -34,18 +38,34 @@ class Savings:
 
 def main():
     print('How long till retirement?')
+    total_num_months = 0
     savings = Savings(0)
-    dri_bonez = Business("DriBonez", 200, 0.05)
-    stone_hackle = Business("Stone Hackle", 500, 0.05)
-    rental1 = Business("rental1", 350, 0.08)
-    rental2 = Business("rental2", 350, 0.08)
-    rental3 = Business("rental3", 350, 0.08)
-    rental4 = Business("rental4", 350, 0.08)
-    rental5 = Business("rental5", 350, 0.08)
+    dri_bonez =     Business("DriBonez", 200, 0.05,0)
+    stone_hackle =  Business("Stone Hackle", 500, 0.05, 2500)
+    rental1 =       Business("rental1", 350, 0.08, 25000)
+    rental2 =       Business("rental2", 350, 0.08, 25000)
+    rental3 =       Business("rental3", 350, 0.08, 25000)
+    rental4 =       Business("rental4", 350, 0.08, 25000)
+    rental5 =       Business("rental5", 350, 0.08, 25000)
+    rental6 =       Business("rental6", 350, 0.08, 25000)
+    rental7 =       Business("rental7", 350, 0.08, 25000)
+    rental8 =       Business("rental8", 350, 0.08, 25000)
+    rental9 =       Business("rental9", 350, 0.08, 25000)
+    rental10 =      Business("rental10", 350, 0.08, 25000)
 
-    while (savings.get_current_savings() <= 2500):
-        savings.add_to_savings(dri_bonez.get_monthly_income())
-        print ("Current Savings: ", savings.get_current_savings())
+    business_array = [dri_bonez, stone_hackle, rental1, rental2, rental3, rental4, 
+                      rental5, rental6, rental7, rental8, rental9, rental10]
+
+
+    for i in range(2): #len(business_array)):
+        while (savings.get_current_savings() <= business_array[i+1].get_start_amount()):
+            savings.add_to_savings(business_array[i].get_monthly_income())
+            total_num_months += 1
+        savings.subtract_from_savings(business_array[i+1].get_start_amount())
+    print("Total number of months: ", total_num_months)
+    print("Total number of years:  ", total_num_months/12)
+    
+    print("amount in savings ", savings.get_current_savings())
     #take the monthlyIncome from a business and add it to the 
     #   Savings:current_monthly_income ONLY once every 12 turns
     #   as well as add to the current savings
